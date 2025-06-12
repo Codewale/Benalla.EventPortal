dd669114ba6723a86c51d30607cbdff1be661a08
kpaulAtSigilo <koushik@sigilotech.com>
2025-06-12 15:35:14 +0530

    feat: change table head color

--- src/app/tickets/[id]/page.tsx
+++ src/app/tickets/[id]/page.tsx
@@ -215,7 +215,7 @@ export default async function TicketPage({ params }) {
           {/* Wrap tables in a scrollable container to prevent overflow */}
           <div className="overflow-x-auto">
             <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-yellow-500">
+              <thead className="bg-gray-100">
                 <tr>
                   <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Item
