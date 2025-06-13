7f7682a9a8fe320e8adf1db2c026271de736650b
kpaulAtSigilo <koushik@sigilotech.com>
2025-06-13 11:53:36 +0530

    fix: server js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
+// server.js
 const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-
-const port = process.env.PORT || 3000; // Change the port to the port that your IIS is running on. Default its 80 and 3000 if you are using it for developing.
-const hostname = "localhost";
-const app = next({ dev, hostname, port });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
@@ -27,12 +25,7 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end("internal server error");
     }
-  })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, async () => {
-      console.log(`> Ready on http://localhost:${port}`);
-    });
+  }).listen(process.env.PORT || 3000, () => {
+    console.log(`> Ready on port ${process.env.PORT || 3000}`);
+  });
 });
