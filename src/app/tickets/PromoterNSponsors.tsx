dc4057d11748bac7be4c08162d1a115e418fa0b5
Joydeep <joydeep@sigilotech.com>
2025-08-25 18:48:05 +0530

    Revert "Revert "Organize file structure and manage file relations""

--- src/app/tickets/PromoterNSponsors.tsx
+++ src/app/tickets/PromoterNSponsors.tsx
@@ -1,28 +1,14 @@
-// "use client";
-
-import { useTicketAndDisplayData } from "@/Hooks/useFetch";
-import { Space_Grotesk, Archivo_Black } from "next/font/google";
-// import axios from "axios";
+import { useTicketAndDisplayData } from "@/hooks/useFetch";
 import React from "react";
-import AlertMessages from "@/Components/Common/Alert";
+import AlertMessages from "@/components/common/Alert";
 
-import SectionHeader from "@/Components/Common/SectionHeader";
-import WhiteContainer from "@/Components/Common/WhiteContainer";
-import Background from "@/Components/Common/Background";
-import EventTitle from "@/Components/Common/EventTitle";
-import Loader from "@/Components/Common/Loader";
-import { archivoBlack, spaceGrotesk } from '@/Fonts/fonts'
+import SectionHeader from "@/components/common/SectionHeader";
+import WhiteContainer from "@/components/common/WhiteContainer";
+import Background from "@/components/common/Background";
+import EventTitle from "@/components/common/EventTitle";
+import Loader from "@/components/common/Loader";
+import { archivoBlack, spaceGrotesk } from '@/fonts/fonts'
 
-function formatTimeFromISOString(isoString, options = {}) {
-    const date = new Date(isoString);
-
-    return date.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-        ...options,
-    });
-}
 
 export default function PromoterNsopnsors({ params }) {
 
