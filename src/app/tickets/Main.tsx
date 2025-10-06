be236c9e2878d35a18eae47d43ea5dc7c34e5b0b
Utsab Sarkar <Utsab7915@sigilotech.com>
2025-10-06 14:28:16 +0530

    Fix Page Footer Title

--- src/app/tickets/Main.tsx
+++ src/app/tickets/Main.tsx
@@ -8,14 +8,14 @@ import { useTicketAndDisplayData } from "@/hooks/useFetch";
 
 
 const pages = [
-    { id: "info", component: dynamic(() => import("./FirstInfo")) },
-    { id: "schedules", component: dynamic(() => import("./Schedules")) },
-    { id: "promoter", component: dynamic(() => import("./PromoterNSponsors")) },
-    { id: "links", component: dynamic(() => import("./TicketLinksnOpenings")) },
-    { id: "askAdam", component: dynamic(() => import("./AskAdam")) },
-    { id: "maps", component: dynamic(() => import("./Maps")) },
-    { id: "bookings", component: dynamic(() => import("./Booking")) },
-    { id: "vehicle", component: dynamic(() => import("./Vehicle")) },
+    { id: "info", component: dynamic(() => import("./FirstInfo")), pageTitle: 'Entry Scan' },
+    { id: "schedules", component: dynamic(() => import("./Schedules")), pageTitle: 'Schedule' },
+    { id: "promoter", component: dynamic(() => import("./PromoterNSponsors")), pageTitle: 'With thanks to' },
+    { id: "links", component: dynamic(() => import("./TicketLinksnOpenings")), pageTitle: 'Information' },
+    { id: "askAdam", component: dynamic(() => import("./AskAdam")), pageTitle: 'Ask Adam' },
+    { id: "maps", component: dynamic(() => import("./Maps")), pageTitle: 'Site maps' },
+    { id: "bookings", component: dynamic(() => import("./Booking")), pageTitle: 'Bookings' },
+    { id: "vehicle", component: dynamic(() => import("./Vehicle")), pageTitle: 'Vehicle details' },
 ];
 
 export default function Main({ params }) {
@@ -37,7 +37,7 @@ export default function Main({ params }) {
         if (ifLoading?.ticketDetails?.bookings.length == 0 && pages[6].id == "bookings") {
             pages.splice(6, 1);
         }
-        if (ifLoading?.ticketDetails?.vehicle.length == 0 && pages[6].id == "vehicle") {
+        if (ifLoading?.ticketDetails?.vehicle.length == 0 && (pages[7]?.id == "vehicle" || pages[6]?.id == "vehicle")) {
             pages.splice(7, 1);
         }
 
@@ -50,6 +50,7 @@ export default function Main({ params }) {
         handleIncrement,
         handleDecrement,
         totalPages: pages.length,
+        pageTitle: pages[pageIndex].pageTitle
     };
     return (
         <>
